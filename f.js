b5cfae21d95c30fdc7fbf367e845ecec37881fc1
erfan1881 <154612197+erfan1881@users.noreply.github.com>
2025-01-09 21:25:21 +0330

    Add files via upload

---
+++ f.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("loadButton");
+
+button.onclick = () => {
+    document.body.classList.add("loaded"); // کاهش شفافیت
+    button.textContent ='💜🤍💜';
+    // بعد از ۱۰۰۰ میلی‌ثانیه (یک ثانیه) کار زیر انجام می‌شود
+    setTimeout(() => {
+        document.body.classList.remove("not-loaded"); // تغییر تصویر پس‌زمینه
+        document.body.style.backgroundImage = "url('new-image.jpg')"; // تصویر جدید
+
+        // پس از تغییر پس‌زمینه، شفافیت را به تدریج به ۱ برمی‌گردانیم
+        setTimeout(() => {
+            document.body.classList.remove("loaded");
+            document.body.style.opacity = 1; // شفافیت دوباره به ۱ برمی‌گردد
+        }, 50); // یک تاخیر جزئی
+    }, 1000); // زمان انتظار برای کاهش شفافیت
+};
